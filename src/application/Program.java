c5da65592e38967c2285420b42332377e808274a
Cezar Detilio <cezardetilio@gmail.com>
2022-05-06 05:14:07 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
-import model.entities.Departament;
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Departament obj = new Departament(1,"Livro");
-		System.out.println(obj);
-	}
 
+		Department obj = new Department(1, "Livro");
+		System.out.println(obj);
+
+		Seller sel = new Seller(2, "Bob", "bob@gmail.com", new Date(), 3.000, obj);
+		System.out.println(sel);
+	}
 }
